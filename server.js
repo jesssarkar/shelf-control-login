3be2a21b96effce5aca2d3283d1515d9e9757f13
Jessica Sarkar <jessicalosee@gmail.com>
2022-09-17 16:42:32 -0500

    added signup

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-//const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
 const bookRoutes = require('./routes/books')
 
 require('dotenv').config({path: './config/.env'})
+
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -22,6 +25,22 @@ app.use(express.json())
 app.use(cors())
 app.use(logger('dev'))
 
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+    })
+  )
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
+app.use('/', mainRoutes)
 app.use('/books', bookRoutes)
 
 app.listen(process.env.PORT, ()=>{
